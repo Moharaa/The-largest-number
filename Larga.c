b0909662d4fcab4807e80d5bb269c9990efde459
Moharaa <137526332+Moharaa@users.noreply.github.com>
2023-06-26 19:45:37 +0330

    Create Larga.c

---
+++ Larga.c
@@ -0,0 +1,21 @@
+<include <iostream#
+<include <conio.h#
+
+;using namespace std
+
+ } ()int main
+;int i,x,max
+    
+  ;cin>>x
+;max = x
+    
+    for(i=2;i<=5;i++)
+    }
+   ;cin>>x
+if(x>max)
+;max = x
+ { 
+    
+;cout<<"max"<<max ;()getch
+;return 0
+{
